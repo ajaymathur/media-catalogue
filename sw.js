b9ceb724725b8f2474be6bf21adc5079f35ac05c
Ajay Narain Mathur <ajaynarainmathur@gmail.com>
2017-08-31 21:28:20 +1000

    Updates

--- sw.js
+++ sw.js
@@ -2,15 +2,15 @@ var cacheName = 'media-catalogue-v1';
 
 var filesToCache = [
     '.',
-    'index.html',
-    'static/js/bundle.js',
-    'static/css/main.css',
-    'static/media/app-store.svg',
-    'static/media/facebook.svg',
-    'static/media/instagram.svg',
-    'static/media/play-store.svg',
-    'static/media/twitter.svg',
-    'static/media/windows-store.svg',
+    'media-catalogue/index.html',
+    'media-catalogue/static/js/bundle.js',
+    'media-catalogue/static/css/main.css',
+    'media-catalogue/static/media/app-store.svg',
+    'media-catalogue/static/media/facebook.svg',
+    'media-catalogue/static/media/instagram.svg',
+    'media-catalogue/static/media/play-store.svg',
+    'media-catalogue/static/media/twitter.svg',
+    'media-catalogue/static/media/windows-store.svg',
 ];
 
 self.addEventListener('install', function(event) {
@@ -21,4 +21,17 @@ self.addEventListener('install', function(event) {
                 return cache.addAll(filesToCache);
             })
     )
-})
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          if (response) {
+            return response;
+          }
+          return fetch(event.request);
+        }
+      )
+    );
+  });
